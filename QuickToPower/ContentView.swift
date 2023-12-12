feedcfd5c8b8ef78036a6c2a3d8ea31616f24455
Sinzolo <77647002+Sinzolo@users.noreply.github.com>
2023-12-12 11:24:55 +0000

    Initial Commit

---
+++ QuickToPower/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickToPower
+//
+//  Created by Melchizedek Gray on 12/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickToPower/QuickToPowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickToPowerApp.swift
+//  QuickToPower
+//
+//  Created by Melchizedek Gray on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuickToPowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
